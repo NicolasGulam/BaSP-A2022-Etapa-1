f6557917470566764e2d94f9a3fea2b3f19ea0f9
NicolasGulam <nicogulam99@gmail.com>
2022-09-17 20:59:29 -0300

    BaSP-W05-2022: Added section d e f of second exercise

--- Semana-05/exercises/02_strings.js
+++ Semana-05/exercises/02_strings.js
@@ -24,3 +24,33 @@ var phrase6, phrase7;
 phrase6 = 'Roses are red, violets are blue'
 phrase7 = phrase6.substring(phrase6.length - 3, phrase6.length);
 console.log(phrase7);
+
+/* d. Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo string con la primera letra en 
+mayúscula y las demás en minúscula. Guardar el resultado en una nueva variable (utilizar substring, toUpperCase, 
+toLowerCase y el operador +).*/
+
+console.log('-Exercise 2.d:');
+var phrase8, phrase9;
+phrase8 = 'Used as an advanced warning. To become keenly aware.';
+phrase9 = phrase8.substring(0,1).toUpperCase() + phrase8.substring(1).toLowerCase();
+console.log(phrase9);
+
+/* e. Crear una variable de tipo string con al menos 10 caracteres y algún espacio en blanco. Encontrar la posición del 
+primer espacio en blanco y guardarla en una variable (utilizar indexOf).*/
+
+console.log('-Exercise 2.e:');
+var phrase10 = 'Teacher of math';
+console.log('The first space is at the location: ' + phrase10.indexOf(' '));
+
+/* f. Crear una variable de tipo string con al menos 2 palabras largas (10 caracteres y algún espacio entre medio). 
+Utilizar los métodos de los ejercicios anteriores para generar un nuevo string que tenga la primera letra de ambas 
+palabras en mayúscula y las demás letras en minúscula (utilizar indexOf, substring, toUpperCase, toLowerCase y el 
+operador +).*/
+
+console.log('-Exercise 2.f:');
+var phrase11, phrase12;
+phrase11 = 'miracle channel';
+phrase12 = phrase11.substring(0,1).toUpperCase() + phrase11.substring(1, phrase11.indexOf(' ') + 1).toLowerCase() + 
+phrase11.substring(phrase11.indexOf(' ') + 1, phrase11.indexOf(' ') + 2).toUpperCase() + 
+phrase11.substring(phrase11.indexOf(' ') + 2).toLowerCase();
+console.log(phrase12);
